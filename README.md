dc2044adf3f183f5e09b50d7fcb2762b081e5e0c
jmasarweh <jamil.masarweh@gmail.com>
2024-06-13 18:32:29 +0100

    Dev (#1)
    First push
    
    * Update README.md
    
    * Update resmed_sleephq_uploader_v1.py
    
    Removed the need to hardcode the TEAM_ID in the env, instead, the module will obtain it dynamically with get_team_id()
    
    add .env info in comments
    
    * Update .env
    
    removed team_id var as it is not needed anymore
    
    * Update resmed_sleephq_uploader_v1.py
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # cpap-sleephq-uploader
 Uploads CPAP data to the SleepHQ Cloud.
+
+
+# Info
+
+The uploader consists of two main executable python3 files:
+
+-ezshare_downloader_v1.py which will download your cpap files stored on an Ezshare Wifi SD Card plugged in to your machine (credit to: @JCOvergaar https://github.com/JCOvergaar)
+
+-resmed_sleephq_uploader_v2.py which will take the dowloaded files from the first module above, and upload them to https://sleepHQ.com
+
+## Prerequisits
+
+-These files requires libraries and syntaxes written in Python3 (Python 3.9.6 as of this readme was last updated). Download your OS copy accordinly.
+
+-For now these files run based on MacOS directories ie: /User/YourUserFolder/Documents/CPAP_Data/SD_Card/. Create a CPAP_Data folder under your User's Document folder. Then create the SD_Card subfolder under CPAP_Data.
+
+-The ezshare_downloader module will delete anything in the SD_Card and DATALOG folders, so make sure you have backed these up. The deletion occurs to ensure clean fresh data is sent to SleepHQ at each run.
+
+## Installation
+
+### Python3 Setup instructions for Mac users
+
+Install HomeBrew (if you don't have it): 
+
+ `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
+ 
+Install Python 3 using HomeBrew: 
+
+ `brew install python`
+ 
+Install additional libraries using python's package installer:
+
+`pip install requests beautifulsoup4`
+
+ Install Python DotEnv so that Python can read your .env file:
+ 
+`pip install python-dotenv`
+
+You may need to use `pip3` in the commands above instead of `pip`, if your homebrew is not pointing to Python3 somehow.
+
+## Populating the .env file
+
+You will first need to grab your API Keys and Secreats from the SleepHQ Account Settings section/API Keys.
+
+`CLIENT_ID` -> This is your Client Id from the SleepHQ Account Settings section/API Keys
+
+`CLIENT_SECRET` -> This is your Client Secret from the SleepHQ Account Settings section/API Keys
+
+`DEVICE_ID` -> (Placeholder, may not be needed)
+
+`SUB_PATH` -> This will be the path to your SD_Card folder, i.e "/Users/John/Documents/CPAP_Data/SD_Card" without the last forward slash
+
+`DIR_PATH`  -> This will be the path to your SD_Card folder, i.e "/Users/John/Documents/CPAP_Data/SD_Card/" WITH the last forward slash
+
+Save the .env file in the same directory of the two py files above.
+
+## Important Info:
+
+The downloader file uses Wifi Switching to connect to the EzShare SSD and disconnect to reconnect to your home/own wifi. This is only supported on MacOs for now.
+
+The modules will only upload the last night's data. It will not upload any prior data. It can be tweaked to handle all the files, but you will realise that for mass uploads, the SleepHQ.com web UI is much faster.
+
+The modules have been tested for the folders and directory structure above. You can have your own paths, as long as the absolute path to the SD_Card folder is consistent with SUB_PATH and DIR_PATH above.
+
+Any tweaks and comments are welcomed.
+
---
+++ resmed_sleephq_uploader_v1.py
@@ -0,0 +1,138 @@
+# ################################################################################################
+# You must install python3 for macOS 10.9 and later: https://www.python.org/downloads/macos/
+# You also must change the variables and Id in the Variables Section of .env file in this repo.
+# #################################################################################################
+
+import requests
+import hashlib
+import os
+import sys
+import pathlib
+import json
+from collections import defaultdict, OrderedDict
+import time
+from dotenv import load_dotenv  # For loading environment variables
+
+# Load environment variables
+load_dotenv()
+
+# team_id = os.getenv('TEAM_ID') ignored for now
+device_id = os.getenv('DEVICE_ID')
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+sub_path = os.getenv('SUB_PATH')
+dir_path = os.getenv('DIR_PATH')
+
+# Get the Access Token from SleepHQ
+def get_access_token(client_id, client_secret):
+    url = "https://sleephq.com/oauth/token"
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'password',
+        'scope': 'read write'
+    }
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+        print("Authorization successful")
+        return 'Bearer ' + response.json()['access_token']
+    except requests.RequestException as e:
+        print(f"Failed to get access token: {e}")
+        sys.exit(1)
+
+# Creates an MD5 Hashed checksum of the files
+def calculate_md5(file_path):
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
+            hasher.update(chunk)
+    return hasher.hexdigest()
+
+# Retrieves your Team Id from Sleep HQ
+def get_team_id(authorization):
+    url = "https://sleephq.com/api/v1/me"
+    headers = {
+        'Authorization': authorization,
+        'Accept': 'application/json'
+    }
+    try:
+        response = requests.request("GET", url, headers=headers)
+        response.raise_for_status()
+        print("Team Id retrieved successfully")
+        return response.json()['data']['current_team_id']
+    except requests.RequestException as e:
+        print(f"Failed to get Team Id: {e}")
+        sys.exit(1)
+
+# Prepares the files for import and adds them to a collection and a JSON dump for 
+# later processing in the request payload
+def collect_files(dir_path, sub_path):
+    all_files = [file for file in pathlib.Path(dir_path).rglob("*") if file.is_file() and not file.name.startswith('.')]
+    import_files = defaultdict(list)
+    for file in all_files:
+        long_path = os.path.abspath(file.parent)
+        final_path = long_path.replace(sub_path, '.')
+        import_files['path'].append(final_path + '/' + file.name)
+        import_files['name'].append(file.name)
+        import_files['content_hash'].append(calculate_md5(file))
+    return import_files
+
+# Obtains an Import Reservation Id from SleepHQ
+def reserve_import_id(team_id, authorization):
+    url = f"https://sleephq.com/api/v1/teams/{team_id}/imports"
+    headers = {'Authorization': authorization, 'Accept': 'application/json'}
+    payload = {'programatic': False}
+    try:
+        response = requests.post(url, headers=headers, data=payload)
+        response.raise_for_status()
+        return response.json()['data']['id']
+    except requests.RequestException as e:
+        print(f"Failed to reserve import ID: {e}")
+        sys.exit(1)
+
+# Uploads the files, one by one to SleepHQ
+def upload_files(import_id, authorization, final_import_files_list, dir_path):
+    url = f"https://sleephq.com/api/v1/imports/{import_id}/files"
+    headers = {'Authorization': authorization}
+    for value in final_import_files_list:
+        os_path = str(value['path']).strip("./")
+        file_path = os.path.join(dir_path, os_path)
+        payload = value
+        files = [('file', (value['name'], open(file_path, 'rb'), 'application/octet-stream'))]
+        try:
+            response = requests.post(url, headers=headers, data=payload, files=files)
+            response.raise_for_status()
+            print(f"File {value['name']} has been imported")
+        except requests.RequestException as e:
+            print(f"Failed to upload file {value['name']}: {e}")
+            sys.exit(1)
+        time.sleep(1.5)
+
+# Closes the Import and starts the processing of the uploaded files
+def process_imported_files(import_id, authorization):
+    url = f"https://sleephq.com/api/v1/imports/{import_id}/process_files"
+    headers = {
+        'Authorization': authorization,
+        'Accept': 'application/json'
+    }
+    try:
+        response = requests.post(url, headers=headers)
+        response.raise_for_status()
+        print(f"Files are now being processed in SleepHQ for Import ID: {import_id}")
+    except requests.RequestException as e:
+        print(f"Failed to process imported files: {e}")
+        print(f"But you can try the Process Import request again later by calling: {url}")
+        sys.exit(1)
+
+
+# Main workflow
+authorization = get_access_token(client_id, client_secret)
+import_files = collect_files(dir_path, sub_path)
+ordered_import_files = [OrderedDict([('path', t), ('name', d), ('content_hash', c)]) for t, d, c in
+                        zip(import_files['path'], import_files['name'], import_files['content_hash'])]
+final_import_files_list = json.loads(json.dumps(ordered_import_files))
+team_id = get_team_id(authorization)
+import_id = reserve_import_id(team_id, authorization)
+upload_files(import_id, authorization, final_import_files_list, dir_path)
+process_imported_files(import_id, authorization)
